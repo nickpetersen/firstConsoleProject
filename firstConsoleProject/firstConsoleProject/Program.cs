8a8b77e6d27f0bffc604822b8c9fcd0763c0a61f
nickpetersen <32842198+nickpetersen@users.noreply.github.com>
2018-10-16 13:30:21 -0500

    Added project files

---
+++ firstConsoleProject/firstConsoleProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace firstConsoleProjectSolution
+{
+    class MainClass
+    {
+        public static void Main(string[] args) // this is a method called "Main". It is called when the program starts.
+        {
+            int num01;
+            int num02;
+
+            Console.Write ("Type a number to be divided: ");
+            num01 = Convert.ToInt32 (Console.ReadLine ());
+            Console.Write("Type another number: ");
+            num02 = Convert.ToInt32 (Console.ReadLine ());
+            Console.WriteLine ("The result is " + num01 * num02);
+            Console.ReadKey();
+        }  
+    }
+}
